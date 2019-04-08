b96e308854cd90800669dd8cc2e314cde8c89e01
sergio.banares <sergio.banares@gmail.com>
2019-04-08 12:29:45 -0300

    Change: Code Rearrange & Adapter logic 002

--- coveritem/src/main/java/com/artear/cover/coveritem/presentation/adapter/DefaultViewHolder.kt
+++ coveritem/src/main/java/com/artear/cover/coveritem/presentation/adapter/DefaultViewHolder.kt
@@ -6,9 +6,7 @@ import com.artear.cover.coveritem.presentation.model.ArtearObject
 import com.artear.cover.coveritem.presentation.model.ArtearSection
 
 
-class DefaultViewHolder(itemView: View, override var listener: ArtearOnClickListener? = null) :
-        RecyclerView.ViewHolder(itemView),
-        ArtearViewHolder<ArtearObject<*>> {
+class DefaultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), ArtearViewHolder<ArtearObject<*>> {
 
     override fun bind(model: ArtearObject<*>, artearSection: ArtearSection) {
         //Empty. Not bind anything
