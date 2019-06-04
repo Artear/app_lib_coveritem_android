8543d842c374e1fa880bd434d5cb1625cc7af5d7
daver1419 <daver1419@gmail.com>
2019-06-04 16:42:13 -0300

    Add: ContainerHeaderDeserializer. Change: ContainerHeader -> title can not be null

--- coveritem/src/main/java/com/artear/cover/coveritem/repository/model/header/ContainerHeader.kt
+++ coveritem/src/main/java/com/artear/cover/coveritem/repository/model/header/ContainerHeader.kt
@@ -1,7 +1,10 @@
 package com.artear.cover.coveritem.repository.model.header
 
+import com.artear.cover.coveritem.repository.deserializer.header.ContainerHeaderDeserializer
+import com.google.gson.annotations.JsonAdapter
 
+@JsonAdapter(ContainerHeaderDeserializer::class)
 data class ContainerHeader(
-    val title: String?,
+    val title: String,
     val style: ContainerHeaderStyle?
 )
