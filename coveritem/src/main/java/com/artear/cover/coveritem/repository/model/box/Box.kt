7473e0d44f990a46be48f546d1fb1732569e7f2f
daver1419 <daver1419@gmail.com>
2019-06-04 17:11:23 -0300

    Change: move box Data to box package

--- coveritem/src/main/java/com/artear/cover/coveritem/repository/model/box/Box.kt
+++ coveritem/src/main/java/com/artear/cover/coveritem/repository/model/box/Box.kt
@@ -1,5 +1,3 @@
 package com.artear.cover.coveritem.repository.model.box
 
-import com.artear.cover.coveritem.repository.model.BoxData
-
 data class Box(var type: BoxType, val style: BoxStyle, val data: BoxData)
