df357771a6c802b2f78c553e460e496ce9d505be
daver1419 <daver1419@gmail.com>
2019-06-05 12:00:55 -0300

    Remove: unnecessary MediaStyleDeserializer. All field are optional

--- coveritem/src/main/java/com/artear/cover/coveritem/repository/model/media/MediaStyle.kt
+++ coveritem/src/main/java/com/artear/cover/coveritem/repository/model/media/MediaStyle.kt
@@ -1,11 +1,8 @@
 package com.artear.cover.coveritem.repository.model.media
 
-import com.artear.cover.coveritem.repository.deserializer.media.MediaStyleDeserializer
 import com.artear.cover.coveritem.repository.model.style.StyleBackground
 import com.artear.cover.coveritem.repository.model.style.StylePlaceholder
 import com.artear.cover.coveritem.repository.model.style.StyleRound
-import com.google.gson.annotations.JsonAdapter
 
 
-@JsonAdapter(MediaStyleDeserializer::class)
 data class MediaStyle(val background: StyleBackground?, val placeholder: StylePlaceholder?, val rounded: StyleRound?)
