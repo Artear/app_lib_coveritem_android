3da9b5494b3fa2192952d84aefe87b4d8c031283
sergiobanares <sergio.banares@gmail.com>
2019-07-11 15:51:07 -0300

    Change: Version 0.2.10         Image Getter From Media Fix

--- stevedoreitems/src/main/java/com/artear/stevedore/stevedoreitems/presentation/model/ArtearItemDecoration.kt
+++ stevedoreitems/src/main/java/com/artear/stevedore/stevedoreitems/presentation/model/ArtearItemDecoration.kt
@@ -5,5 +5,9 @@ class ArtearItemDecoration(
         var marginTop: Int = 0,
         var marginLeft: Int = 0,
         var marginBottom: Int = 0,
-        var marginRight: Int = 0
+        var marginRight: Int = 0,
+        var paddingTop: Int = 0,
+        var paddingLeft: Int = 0,
+        var paddingBottom: Int = 0,
+        var paddingRight: Int = 0
 )
